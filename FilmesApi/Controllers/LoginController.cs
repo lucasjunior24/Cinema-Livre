91109820846b4c31b5e85811b6cda0608b44907e
lucas souza <lucas24souza24@outlook.com>
2022-01-26 16:22:24 -0400

    Criado UserDbContext

---
+++ FilmesApi/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using FilmesApi.Data.Dtos.Requests;
+using FilmesApi.Services;
+using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private LoginService loginService;
+
+        public LoginController(LoginService loginService)
+        {
+            this.loginService = loginService;
+        }
+
+        [HttpPost]
+        public IActionResult LogaUsuario(LoginRequest loginRequest)
+        {
+            Result result = loginService.LogaUsuario(loginRequest);
+
+            if (result.IsFailed) return StatusCode(500);
+            return Ok();
+        }
+    }
+}
---
+++ FilmesApi/Services/LoginService.cs
@@ -0,0 +1,28 @@
+﻿using FilmesApi.Data.Dtos.Requests;
+using FluentResults;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Services
+{
+    public class LoginService
+    {
+        private SignInManager<IdentityUser<int>> signInManager;
+
+        public LoginService(SignInManager<IdentityUser<int>> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        internal Result LogaUsuario(LoginRequest loginRequest)
+        {
+            var result = signInManager.PasswordSignInAsync(loginRequest.Username, loginRequest.Password, false, false);
+
+            if (result.Result.Succeeded) return Result.Ok();
+            return Result.Fail("Login falhou");
+        }
+    }
+}
